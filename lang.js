612d5bf335cc7ac89b376007482b0add17dca759
Marcelo Forclaz <chechoforclaz@gmail.com>
2017-05-31 12:01:06 -0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,26 +1,20 @@
 $(function() {
-    var defaultLang = "es";
-    var languages = {
-        "es": {
-            HELLO_LABEL: "Hola Mundo!"
-        },
+    var defaultLang = "es"; // Define default language
+  
+    $.getJSON('/lang.json', function( data ) { // Replace lang.json by your own url language json file.
+        var languages = data;
+        translate(defaultLang);
 
-        "en": {
-            HELLO_LABEL: "Hello World"
+        function translate(lang) {
+            $('[data-translate]').each(function() {
+                $(this).text(languages[lang][$(this).data('translate')]);
+            });
         }
-    };
-    
-    translate(defaultLang);
 
-    function translate(lang) {
-        $('[data-translate]').each(function() {
-            $(this).text(languages[lang][$(this).data('translate')]);
+        $('.lang').click(function(e) {
+            e.preventDefault();
+            var lang = $(this).data('lang');
+            translate(lang);
         });
-    }
-
-    $('.lang').click(function(e) {
-        e.preventDefault();
-        var lang = $(this).data('lang');
-        translate(lang);
     });
 });
